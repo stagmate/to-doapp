325a8efadf29049fde3a6e7db527a61125f19082
stagmate <44667254+stagmate@users.noreply.github.com>
2020-05-01 17:32:57 +0530

    Initial commit

---
+++ welcome.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+if (strlen($_SESSION['id']==0)) {
+  header('location:logout.php');
+  } else{
+ 
+    // initialize errors variable
+	$errors = "";
+
+	// connect to database
+	$db = mysqli_connect("localhost", "root", "", "todo");
+
+	// insert a quote if submit button is clicked
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['task'])) {
+			$errors = "You must fill in the task";
+		}else{
+			$task = $_POST['task'];
+			$sql = "INSERT INTO tasks (task) VALUES ('$task')";
+			mysqli_query($db, $sql);
+			header('location: welcome.php');
+		}
+	}	
+if (isset($_GET['del_task'])) {
+	$id = $_GET['del_task'];
+
+	mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
+	header('location: welcome.php');
+}	
+	  
+	
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+	<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+       
+        
+
+        <!-- Bootstrap -->
+        <link href="css/main.css" rel="stylesheet">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    <title>Welcome </title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/heroic-features.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Welcome !</a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#"><?php echo $_SESSION['name'];?></a>
+                    </li>
+                    <li>
+                        <a href="logout.php">Logout</a>
+                    </li>
+                  
+                </ul>
+            </div>
+        </div>
+    </nav>
+	<?php
+     echo   date("l"). "&nbsp&nbsp&nbsp" . date("d/m/Y")."<br>" ;
+	 ?>
+	 <div class="container">
+    	<h2 class="text-center">Todo List App</h2>
+
+        	<div class="form-group">
+        		<label for="itemInput">Add Item</label>
+        		<input type="text" class="form-control" name="" id="itemInput" >
+        	</div>
+                <button id="addButton" class="btn btn-primary">Add To List</button>
+                <button id="clearButton" class="btn btn-danger">Clear Todo List</button>
+        <h3>Todo List</h3>
+        <ul id="todoList"></ul>
+
+    </div>
+
+
+            <p><a  href="logout.php" class="btn btn-primary btn-large">Logout</a>
+            </p>
+        </header>
+        <hr>
+
+      
+
+
+        </div>
+
+        <hr>
+
+
+    </div>
+	 <script type="text/javascript" src="js/main.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
+<?php } ?>
